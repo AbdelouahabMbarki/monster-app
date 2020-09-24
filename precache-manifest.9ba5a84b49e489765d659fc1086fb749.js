ddcbc8a5eba9b9d8820a62161981d2cb7269afb7
h1rkul2 <dev@h1rkul2.com>
2020-09-24 03:13:15 +0000

    Updates

---
+++ precache-manifest.9ba5a84b49e489765d659fc1086fb749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4183ddb520a5b57fa6d18b78618b1e",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "9d6950d1d065513604cf",
+    "url": "/monster-app/static/css/main.eb4a47df.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6950d1d065513604cf",
+    "url": "/monster-app/static/js/main.31f89d8f.chunk.js"
+  },
+  {
+    "revision": "70ffa8869a950765e1c3",
+    "url": "/monster-app/static/js/runtime-main.4edd5f74.js"
+  }
+]);
